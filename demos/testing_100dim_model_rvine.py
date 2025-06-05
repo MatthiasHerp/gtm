5dd94725479801c9b634ebc821b388520206e9c0
Matthias Herp <matze.herp@web.de>
2025-06-05 10:14:57 +0200

    added cuda able testing script for 100 dimensional vine structure

---
+++ demos/testing_100dim_model_rvine.py
@@ -0,0 +1,86 @@
+from gtm import *
+
+# Sample Copulas Package
+import pyvinecopulib as pv
+# Helpers to generate specfific vines and analyze there conditional independencies
+from demos.pyvinecopulib_simulation_helpers import *
+
+# Other Stuff
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
+from demos.dataset_helpers import Generic_Dataset
+from sklearn.metrics import roc_auc_score, roc_curve
+
+if __name__ == "__main__":
+    
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    
+    D = 100  # dimension
+    rvine_structure = pv.RVineStructure.simulate(D)
+    pair_copulas = sample_random_pair_copulas(D,Independence_tree=3)
+    vine_model = pv.Vinecop.from_structure(structure=rvine_structure, pair_copulas=pair_copulas)
+    
+    # Train
+    N_train = 10000
+    simulated_data_uniform_train = vine_model.simulate(n=N_train)
+    simulated_data_train = torch.distributions.Normal(0,1).icdf(torch.tensor(simulated_data_uniform_train)).float()
+
+    # Validate
+    N_validate = 10000
+    simulated_data_uniform_validate = vine_model.simulate(n=N_validate)
+    simulated_data_validate = torch.distributions.Normal(0,1).icdf(torch.tensor(simulated_data_uniform_validate)).float()
+    
+    # Create dataset and DataLoader
+    dataset_train = Generic_Dataset(simulated_data_train)
+    dataloader_train = DataLoader(dataset_train, batch_size=N_train)
+
+    dataset_validate = Generic_Dataset(simulated_data_validate)
+    dataloader_validate = DataLoader(dataset_validate, batch_size=N_validate)
+    
+    model = GTM(
+                transformation_spline_range=list([[-10], [10]]), 
+                degree_decorrelation=40,
+                degree_transformations=10,
+                num_decorr_layers=3,
+                num_trans_layers=1,
+                number_variables=100,
+                spline_decorrelation="bspline",
+                spline_transformation="bspline",
+                device=device) 
+                
+    model.to(device)                       
+                
+                
+    #study = model.hyperparameter_tune_penalties( 
+    #                            train_dataloader=dataloader_train, 
+    #                            validate_dataloader=dataloader_validate, 
+    #                            penvalueridge = 0,
+    #                            penfirstridge = "sample",
+    #                            pensecondridge = "sample",
+    #                            ctm_pensecondridge = "sample",
+    #                            lambda_penalty_params = 0,
+    #                            adaptive_lasso_weights_matrix=False,
+    #                            iterations=1000, 
+    #                            patience=20, 
+    #                            min_delta=1e-8, 
+    #                            optimizer='LBFGS', 
+    #                            pretrained_transformation_layer=True,
+    #                            n_trials=40)
+    
+    penalty_params=torch.FloatTensor([
+                            0, #study.best_params["penvalueridge"],
+                            0, #study.best_params["penfirstridge"],
+                            0, #study.best_params["pensecondridge"],
+                            0, #study.best_params["ctm_pensecondridge"]
+                            ])
+
+    # pretrain the marginal transformations
+    _ = model.pretrain_tranformation_layer(dataloader_train, iterations=1000)
+    
+    # train the joint model
+    _ = model.train(train_dataloader=dataloader_train, validate_dataloader=dataloader_validate, 
+                        iterations=1000, optimizer="LBFGS",
+                        penalty_params=penalty_params)
+    
+                
