fixing gradients in likelihood, posterior + VI gradients should be still fixed

gtm/gtm_layers/decorrelation_layer.py
@@ -188,7 +188,9 @@ class Decorrelation(nn.Module):
         )
         return {
             "output": input.clone(),  # .requires_grad_(True),
-            "log_d": torch.zeros(input.size(), device=self.device),  # .requires_grad_(True),
+            "log_d": torch.zeros(
+                input.size(), device=self.device
+            ),  # .requires_grad_(True),
             "transformation_second_order_ridge_pen_sum": 0,
             "second_order_ridge_pen_sum": 0,
             "first_order_ridge_pen_sum": 0,
@@ -198,7 +200,7 @@ class Decorrelation(nn.Module):
             # "der2_lambda_matrix": lambda_matrix_general.clone()
         }
 
-    def compute_starting_values_bspline(self, start_value=0.001) -> Tensor:
+    def compute_starting_values_bspline(self, start_value=0.001):
         p = torch.FloatTensor(
             np.repeat(
                 np.repeat(start_value, self.degree + self.spline_order - 1),
@@ -207,16 +209,15 @@ class Decorrelation(nn.Module):
         )
 
         if self.num_lambdas == 1:
-            return nn.Parameter(p.unsqueeze(1))
-        
+            params = nn.Parameter(p.unsqueeze(1))
         else:
-            
-            return nn.Parameter(
+            params = nn.Parameter(
                 torch.reshape(
                     p, (self.degree + self.spline_order - 1, int(self.num_lambdas))
                 )
             )
 
+        return params
 
     def bspline_prediction_method(
         self,
@@ -488,12 +489,16 @@ class Decorrelation(nn.Module):
                             multi=True,
                         )
 
-                        return_dict["second_order_ridge_pen_sum"] = return_dict["second_order_ridge_pen_sum"] + second_order_ridge_pen_current
-                        return_dict["first_order_ridge_pen_sum"] = return_dict["first_order_ridge_pen_sum"] + first_order_ridge_pen_current
-                        return_dict["param_ridge_pen_sum"] = return_dict["param_ridge_pen_sum"] + param_ridge_pen_current
+                        return_dict[
+                            "second_order_ridge_pen_sum"
+                        ] += second_order_ridge_pen_current
+                        return_dict[
+                            "first_order_ridge_pen_sum"
+                        ] += first_order_ridge_pen_current
+                        return_dict["param_ridge_pen_sum"] += param_ridge_pen_current
 
-                    if return_scores_hessian:
-                        if self.params_multiplier:
+                    if return_scores_hessian == True:
+                        if self.params_multiplier is not False:
                             warnings.warn(
                                 "Warning: return_scores_hessian not implemented for multiplicative effect. The der_lambda_matrix and der2_lambda_matrix will be wrong."
                             )
@@ -523,9 +528,10 @@ class Decorrelation(nn.Module):
                         der_lambda_value = 0
                         der2_lambda_value = 0
 
-                    if self.params_multiplier:
-                        lambda_value_multiplier_total = lambda_value_multiplier_total * lambda_value_multiplier
-                    
+                    if self.params_multiplier is not False:
+                        lambda_value_multiplier_total = (
+                            lambda_value_multiplier_total * lambda_value_multiplier
+                        )
                     lambda_value_total += lambda_value * input[:, covar_num]
 
                     params_index += 1
@@ -538,11 +544,18 @@ class Decorrelation(nn.Module):
                     # return_dict["der2_lambda_matrix"][:, var_num, covar_num] = der_lambda_value + der2_lambda_value * input[:, covar_num] + der_lambda_value
 
                 # filling in the multiplicative effect into the lambda matrix
-                return_dict["lambda_matrix"][:, var_num, var_num] = lambda_value_multiplier_total
+                return_dict["lambda_matrix"][
+                    :, var_num, var_num
+                ] = lambda_value_multiplier_total
 
-                return_dict["output"][:, var_num] = lambda_value_multiplier_total * input[:, var_num] + lambda_value_total
-                
-                return_dict["log_d"][:, var_num] += torch.log(torch.abs(lambda_value_multiplier_total))
+                return_dict["output"][:, var_num] = (
+                    lambda_value_multiplier_total * input[:, var_num]
+                    + lambda_value_total
+                )
+
+                return_dict["log_d"][:, var_num] += torch.log(
+                    torch.abs(lambda_value_multiplier_total)
+                )
 
         return return_dict
 
@@ -556,11 +569,9 @@ class Decorrelation(nn.Module):
         return_penalties=False,
         return_scores_hessian=False,
     ):
-        
-        use_loop : bool = inverse or self.number_variables == 2 or not self.vmap
 
-        if use_loop:
-            return self.for_loop_forward(
+        if inverse == True or self.number_variables == 2 or self.vmap == False:
+            return_dict = self.for_loop_forward(
                 input,
                 log_d,
                 covariate,
@@ -569,11 +580,12 @@ class Decorrelation(nn.Module):
                 return_scores_hessian=return_scores_hessian,
             )
         # elif self.vmap == True and self.number_variables > 2:
-        
         else:
-            return self.vmap_forward(
+            return_dict = self.vmap_forward(
                 input, log_d, covariate, return_penalties=return_penalties
             )
+
+        return return_dict
 
     def __repr__(self):
         return f"Decorrelation(degree={self.degree}, number_variables={self.number_variables}, spline_range={self.spline_range}, spline={self.spline}, span_factor={self.span_factor}, span_restriction={self.span_restriction}, number_covariates={self.number_covariates}, vectorised_mapping={self.vmap})"

gtm/gtm_model/gtm.py
@@ -557,7 +557,8 @@ class GTM(nn.Module):
                     )
 
             return return_dict_nf_mctm
-    def latent_space_representation(self, y) -> Tensor:
+
+    def latent_space_representation(self, y):
         """
         Returns the fully transformed latent space Z for a given input Y. Z is distributed as a Gaussian N(0,I).
 
@@ -578,13 +579,16 @@ class GTM(nn.Module):
         return_dict = self.forward(y)  # , covariate) #, train=False, evaluate=True)
         return return_dict["output"]
 
-    def __log_likelihood_loss__(self, y: Tensor, mean_loss: bool=True) -> dict[str, Tensor|float|None]:
+    def __log_likelihood_loss__(self, y, mean_loss=True):
         # covariate=False, train=True, evaluate=True,
 
-        return_dict_nf_mctm: dict[str, Tensor|float|None] = log_likelihood(model=self, samples=y, mean_loss=mean_loss)  
-        # train_covariates=covariate, train=train, evaluate=evaluate,
+        return_dict_nf_mctm = log_likelihood(
+            model=self, samples=y, mean_loss=mean_loss
+        )  # train_covariates=covariate, train=train, evaluate=evaluate,
 
-        return_dict_nf_mctm["negative_log_likelihood_data"] = -1*return_dict_nf_mctm["log_likelihood_data"]
+        return_dict_nf_mctm["negative_log_likelihood_data"] = (
+            -1 * return_dict_nf_mctm["log_likelihood_data"]
+        )
 
         return return_dict_nf_mctm
 

gtm/gtm_splines/bspline_prediction_vectorized.py
@@ -1,9 +1,10 @@
+
 import numpy as np
 import torch
-from torch import Tensor
 
 from gtm.gtm_splines.bernstein_basis import kron
-from gtm.gtm_splines.splines_utils import (ReLULeR, adjust_ploynomial_range, custom_sigmoid)
+from gtm.gtm_splines.splines_utils import (ReLULeR, adjust_ploynomial_range,
+                                           custom_sigmoid)
 
 ##################################################################################################################################################################################################################
 ########################## Naive ##########################
@@ -193,7 +194,7 @@ def Naive_Basis(x, spline_range, degree, span_factor, knots, derivativ=0, order=
 
 def compute_multivariate_bspline_basis(
     input, degree, spline_range, span_factor, knots, covariate=False, derivativ=0
-) -> Tensor:
+):
     # We essentially do a tensor prodcut of two splines! : https://en.wikipedia.org/wiki/Bernstein_polynomial#Generalizations_to_higher_dimension
 
     bspline_basis_list = []
@@ -333,16 +334,16 @@ def deboor_algorithm_varying_degrees_first_derivativ(x, k, t, c, p):
     return q[:, p - 1]
 
 
-def run_deBoor_varying_degrees(x, t, c, p, d) -> Tensor:
+def run_deBoor_varying_degrees(x, t, c, p, d):
     # Compute knot indices
-    k: Tensor = compute_k_varying_degrees(x, t)  # , n)
+    k = compute_k_varying_degrees(x, t)  # , n)
 
     if d == 0:
         # Compute B-spline outputs
-        prediction: Tensor = deboor_algorithm_varying_degrees(x, k, t, c, p).squeeze()
+        prediction = deboor_algorithm_varying_degrees(x, k, t, c, p).squeeze()
     elif d == 1:
         # Compute B-spline outputs
-        prediction: Tensor = deboor_algorithm_varying_degrees_first_derivativ(
+        prediction = deboor_algorithm_varying_degrees_first_derivativ(
             x, k, t, c, p
         ).squeeze()
 
@@ -553,63 +554,58 @@ def run_deBoor_fixed_degrees(x, t, c, p, d):
 
 # Bspline Prediction using the deBoor algorithm
 def bspline_prediction_vectorized(
-    params_a: Tensor,
-    input_a: Tensor,
-    knots: Tensor,
-    degree: list[int]|int,
-    spline_range: Tensor,
-    monotonically_increasing: bool=False,
-    derivativ: int=0,
-    return_penalties:bool=False,
-    calc_method: str="deBoor",  # 'Naive_Basis', #before: deBoor
-    span_factor: float=0.1,
-    span_restriction: str="reluler",
-    covariate: bool=False,
-    params_covariate: bool=False,
-    covariate_effect: str="multiplicativ",
-    penalize_towards: float=0,
-    order:int =3,
-    varying_degrees: bool=True,
-    params_a_mask: bool=None,
+    params_a,
+    input_a,
+    knots,
+    degree,
+    spline_range,
+    monotonically_increasing=False,
+    derivativ=0,
+    return_penalties=False,
+    calc_method="deBoor",  # 'Naive_Basis', #before: deBoor
+    span_factor=0.1,
+    span_restriction="reluler",
+    covariate=False,
+    params_covariate=False,
+    covariate_effect="multiplicativ",
+    penalize_towards=0,
+    order=3,
+    varying_degrees=True,
+    params_a_mask=None,
 ):  # device=None
 
-    input_a_clone: Tensor = input_a
+    input_a_clone = input_a
 
     if span_restriction == "sigmoid":
-    
-        input_a_clone: Tensor = custom_sigmoid(input=input_a_clone, polynomial_range=spline_range)
-    
+        input_a_clone = custom_sigmoid(input_a_clone, spline_range)
     elif span_restriction == "reluler":
-    
-        reluler: ReLULeR = ReLULeR(polynomial_range=spline_range)
+        reluler = ReLULeR(spline_range)
         input_a_clone = reluler.forward(input_a_clone)
-
+    else:
+        pass
 
     if calc_method == "deBoor":
 
-        if varying_degrees:
-            prediction: Tensor = run_deBoor_varying_degrees(
+        if varying_degrees == True:
+            prediction = run_deBoor_varying_degrees(
                 x=input_a_clone.T, t=knots.T, c=params_a.T, p=order, d=derivativ
             )
         else:
-            prediction: Tensor = run_deBoor_fixed_degrees(
+            prediction = run_deBoor_fixed_degrees(
                 x=input_a_clone.T, t=knots.T, c=params_a.T, p=order, d=derivativ
             )
     elif calc_method == "Naive":
-        
-        if varying_degrees:
-            prediction: Tensor = Naive_varying_degree(
+        if varying_degrees == True:
+            prediction = Naive_varying_degree(
                 x=input_a_clone.T, t=knots.T, c=params_a, p=order, d=derivativ
             )
-            
         else:
-            prediction: Tensor = Naive_fixed_degree(
+            prediction = Naive_fixed_degree(
                 x=input_a_clone.T, t=knots.T, c=params_a, p=order, d=derivativ
             )
 
     # Adding Covariate in a GAM manner
-    if covariate:
-        
+    if covariate is not False:
         params_covariate_restricted = params_covariate.clone().contiguous()
 
         if order == 2:
@@ -617,10 +613,10 @@ def bspline_prediction_vectorized(
         elif order == 3:
             n = degree + 2
 
-        knots_covariate: Tensor = torch.linspace(
-            start= 0 - order * 1,
-            end=1 + order * 1,
-            steps=n + 4,
+        knots_covariate = torch.linspace(
+            0 - order * 1,
+            1 + order * 1,
+            n + 4,
             dtype=torch.float32,
             device=input_a.device,
         )
@@ -630,28 +626,26 @@ def bspline_prediction_vectorized(
         )
 
         prediction = prediction * prediction_covariate
-    
+
     if return_penalties:
-        
-        if not varying_degrees:
-            second_order_ridge_pen: Tensor = torch.sum(torch.diff(params_a, n=2, dim=0) ** 2)
-            first_order_ridge_pen: Tensor = torch.sum(torch.diff(params_a, n=1, dim=0) ** 2)
-            param_ridge_pen: Tensor = torch.sum(
+        if varying_degrees == False:
+            second_order_ridge_pen = torch.sum(torch.diff(params_a, n=2, dim=0) ** 2)
+            first_order_ridge_pen = torch.sum(torch.diff(params_a, n=1, dim=0) ** 2)
+            param_ridge_pen = torch.sum(
                 (params_a - penalize_towards) ** 2
             )  # penalize_towards
         else:
-            second_order_ridge_pen: Tensor = torch.sum(
+            second_order_ridge_pen = torch.sum(
                 torch.diff(params_a, n=2, dim=0)[params_a_mask[2:, :].bool()] ** 2
             )
-            first_order_ridge_pen: Tensor = torch.sum(
+            first_order_ridge_pen = torch.sum(
                 torch.diff(params_a, n=1, dim=0)[params_a_mask[1:, :].bool()] ** 2
             )
-            param_ridge_pen: Tensor = torch.sum(
+            param_ridge_pen = torch.sum(
                 (params_a - penalize_towards)[params_a_mask.bool()] ** 2
             )
-            
         # Adding Covariate parameter penalisation values
-        if covariate:
+        if covariate is not False:
             second_order_ridge_pen += torch.sum(
                 torch.diff(params_covariate_restricted, n=2, dim=0) ** 2
             )
@@ -662,6 +656,11 @@ def bspline_prediction_vectorized(
                 (params_covariate_restricted - penalize_towards) ** 2
             )  # penalize_towards
 
-        return prediction.T, second_order_ridge_pen, first_order_ridge_pen, param_ridge_pen,
-        
-    return prediction.T
+        return (
+            prediction.T,
+            second_order_ridge_pen,
+            first_order_ridge_pen,
+            param_ridge_pen,
+        )
+    else:
+        return prediction.T

gtm/gtm_splines/splines_utils.py
@@ -1,7 +1,8 @@
+
 import warnings
 
 import torch
-from torch import nn, Tensor
+from torch import nn
 
 
 def adjust_ploynomial_range(polynomial_range: object, span_factor: object) -> object:
@@ -18,7 +19,7 @@ def adjust_ploynomial_range(polynomial_range: object, span_factor: object) -> ob
 
 
 class ReLULeR:  # nn.Module
-    def __init__(self, polynomial_range: Tensor):
+    def __init__(self, polynomial_range: torch.Tensor):
         # super().__init__()
         self.polynomial_range = (
             0.9 * polynomial_range
@@ -42,15 +43,18 @@ class ReLULeR:  # nn.Module
         return x_well_behaved
 
 
-def custom_sigmoid(input: Tensor, polynomial_range: Tensor) -> Tensor:
-    
-    input_01: Tensor = (input - polynomial_range[0]) / (
+def custom_sigmoid(input: torch.Tensor, polynomial_range: torch.Tensor):
+    input_01 = (input - polynomial_range[0]) / (
         polynomial_range[1] - polynomial_range[0]
     )
-    input_11: Tensor = input_01 * 2 - 1
-    input_bounded_01: Tensor = 1 / (1 + torch.exp(-input_11 * 4))
+    input_11 = input_01 * 2 - 1
+    input_bounded_01 = 1 / (1 + torch.exp(-input_11 * 4))
+    input_bounded = (
+        input_bounded_01 * (polynomial_range[1] - polynomial_range[0])
+        + polynomial_range[0]
+    )
 
-    return input_bounded_01 * (polynomial_range[1] - polynomial_range[0]) + polynomial_range[0]
+    return input_bounded
 
 
 if __name__ == "__main__":