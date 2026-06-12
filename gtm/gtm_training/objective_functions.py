e7bc56a0c3238f7f7953174c65849a3ce6c76363
Matthias Herp <matze.herp@web.de>
2026-06-12 12:27:12 +0200

    adding synth samples for the lambda penalty for testing

--- gtm/gtm_training/objective_functions.py
+++ gtm/gtm_training/objective_functions.py
@@ -77,11 +77,32 @@ def training_objective(
             torch.transpose(return_dict_model_loss["lambda_matrix_global"], 1, 2),
             return_dict_model_loss["lambda_matrix_global"],
         )
+        
+        ################################################################################
+        #### New ####
+        ################################################################################
+        
+        synth_samples = 1000
+        z_tilde_gauss = (torch.distributions.Normal(0, 1)
+                .sample((synth_samples, model.number_variables))
+                .to(device=model.device))
+        return_dict_z_tilde_gauss = model.forward(
+            z_tilde_gauss, return_lambda_matrix=True
+        )  # covariate=covariate, evaluate=True, train=False, return_lambda_matrix=True)
+
+        precision_matrix_synth = torch.matmul(
+            torch.transpose(return_dict_z_tilde_gauss["lambda_matrix_global"], 1, 2),
+            return_dict_z_tilde_gauss["lambda_matrix_global"],
+        )
+        precision_matrix = torch.concatenate([precision_matrix,precision_matrix_synth],dim=0)
+        ################################################################################
+        ################################################################################
+        
         if adaptive_lasso_weights_matrix is False:
             pen_lambda_lasso = (
                 (
                     lambda_penalty_params
-                    * 1 / samples.size(0) * (torch.square(precision_matrix).sum(0)) ** 0.5
+                    * 1 / precision_matrix.size(0) * (torch.square(precision_matrix).sum(0)) ** 0.5
                 )
                 .tril(-1)
                 .mean()
